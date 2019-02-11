1ce57550b5a929c6fea08821822d001b3e133890
Mohammed Jubel <jubelm@hotmail.co.uk>
2019-02-11 11:53:08 +0000

    seeded articles table

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -2,9 +2,20 @@ const {
   articleData, commentData, topicData, userData,
 } = require('../data');
 
+const { formatArticles } = require('../utils');
+
 
 exports.seed = (knex, Promise) => knex.migrate
   .rollback()
   .then(() => knex.migrate.latest())
   .then(() => knex.insert(topicData).into('topics'))
-  .then(() => knex('users').insert(userData));
+  .then(() => knex.insert(userData).into('users'))
+  .then(() => {
+    const formattedArticles = formatArticles(articleData);
+    return knex.insert(formattedArticles).into('articles');
+  })
+  .then(() => knex.insert(commentData).into('comments'));
+// .then(() => knex.insert(articleData).into('articles'));
+
+
+console.log(commentData);//   console.log;
